524709324f138f3d7a4ab80a2c99fdfde8f7d43a
Chewycide <gitchewycide@gmail.com>
2022-08-26 22:33:20 +0800

    Add StartWindow widgets

--- window.py
+++ window.py
@@ -1,4 +1,9 @@
-from PyQt5.QtWidgets import QWidget, QDesktopWidget
+from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QGridLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton)
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+LOGO_WIDTH = 150
+LOGO_HEIGHT = LOGO_WIDTH
 
 class StartWindow(QWidget):
 
@@ -7,13 +12,14 @@ class StartWindow(QWidget):
         super().__init__()
 
         self.initWindow()
+        self.initUI()
         self.show()
 
     
     def initWindow(self):
         """Initialize window"""
 
-        self.setMinimumSize(800, 600)
+        self.setFixedSize(475, 350)
         self.setWindowTitle("Login")
 
 
@@ -25,3 +31,35 @@ class StartWindow(QWidget):
 
     def initUI(self):
         """Initialize layouts and widgets"""
+
+        grid = QGridLayout()
+        grid.setContentsMargins(50, 10, 50, 10)
+        grid.setSpacing(1)
+        logo_hbox = QHBoxLayout()
+        button_hbox = QHBoxLayout()
+        
+
+        self.pixela_logo = QLabel("", self)
+        self.pixela_logo.setMaximumSize(LOGO_WIDTH, LOGO_HEIGHT)
+        self.pixela_logo.setPixmap(QPixmap("assets/PIXELA_ORIGINAL.png").scaled(LOGO_WIDTH,LOGO_HEIGHT,Qt.KeepAspectRatio))
+
+
+        self.usern_label = QLabel("Username", self)
+        self.usern_label.setMaximumSize(75, 20)
+
+
+        self.usern_entry = QLineEdit()
+        self.usern_entry.setMaximumSize(150, 20)
+
+
+        self.login_button = QPushButton("Login")
+        self.login_button.setFixedSize(60, 40)
+
+
+        grid.addLayout(logo_hbox, 0, 0, 1, 2)
+        logo_hbox.addWidget(self.pixela_logo)
+        grid.addWidget(self.usern_label, 1, 0)
+        grid.addWidget(self.usern_entry, 1, 1)
+        grid.addLayout(button_hbox, 2, 0, 1, 2)
+        button_hbox.addWidget(self.login_button)
+        self.setLayout(grid)
